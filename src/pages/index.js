b765f42f97d25c227f988b90690100b0c717b568
Анна Каренина <annakarenina@MBP-Anna.Dlink>
2019-07-30 08:04:23 +0300

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,38 @@
 import React from "react"
+// import { Link } from "gatsby";
+import cl from "./../component/Custom.module.css"
 
-export default () => <div>Hello world!</div>
+
+
+
+
+export default () => (
+  <div className={cl.mainWrapper} >
+    <div className={cl.fronttitle} >
+      <h2>Санкт-Петербургская </h2>
+      <h2>Гильдия трубочистов</h2>
+      <h3>ЭКСПЕРТНОСТЬ. ТРАДИЦИИ. ПРИЗНАНИЕ.</h3>
+    </div>
+
+
+
+
+
+
+
+
+  </div>
+);
+
+
+// <div>
+//   <ul>
+//     <li>
+//       <Link to="/aboutus">about</Link>
+//     </li>
+//     <li>
+//       <Link to="/price">about</Link>
+//     </li>
+//
+//   </ul>
+// </div>
